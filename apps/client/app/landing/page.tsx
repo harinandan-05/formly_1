bd3018d42920edd3413833372416f3ec48679d2a
Harinandan <harinandannn@gmail.com>
2026-01-29 22:56:44 +0530

    made the brand partnes banner

--- apps/client/app/landing/page.tsx
+++ apps/client/app/landing/page.tsx
@@ -1,6 +1,5 @@
 "use client"
-import doodle4 from '../../public/doodle4.jpeg'
-import Image from "next/image"
+import { DiPostgresql ,DiStackoverflow ,DiNpm ,DiApple ,DiSwift,DiSass,DiUnitySmall,DiPhp } from "react-icons/di";
 
 export default function Landing(){
     
@@ -50,7 +49,44 @@ export default function Landing(){
                             <button className="bg-yellow-300 p-2 font-semibold mt-4 rounded-md self-center md:self-center">
                                 Create a free Form  ››
                             </button>
+                            <p className='text-center text-sm md:text-md'>
+                                No signup required
+                            </p>
+                            <p className="text-center text-sm mt-20 md:text-md ">
+                                Powering 500,000+ teams at the world’s best companies
+                            </p>
                         </div>
+                </div>
+            </section>
+
+            {/* partners icons section */}
+
+            <section className="w-full mt-8">
+                <div className="w-full max-w-7xl mx-auto flex gap-15 px-50">
+                    <div className="">
+                        <DiPostgresql size={60} />
+                    </div>
+                    <div>
+                        <DiApple size={60} />
+                    </div>
+                    <div>
+                        <DiNpm size={60}  />
+                    </div>
+                    <div>
+                        <DiStackoverflow  size={60} />
+                    </div>
+                    <div>
+                        <DiSwift size={60}/>
+                    </div>
+                    <div>
+                        <DiSass size={60}/>
+                    </div>
+                    <div>
+                        <DiPhp size={60}/>
+                    </div>
+                    <div>
+                        <DiUnitySmall size={60}/>
+                    </div>
                 </div>
             </section>
         </div>
